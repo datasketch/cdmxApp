66630bc581dcdda3eebadaca33be0e8a167dd038
Camila Achuri <camilaachury.4@gmail.com>
2022-03-28 13:37:36 -0500

    add filters inputs style

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,7 +17,7 @@
 ## Add one line by package you want to add as dependency
 
 list_pkg <- c("shinydisconnect", 
-              "shinybusy", "DT", "airtabler")
+              "shinybusy", "DT", "airtabler", "shinyWidgets")
 purrr::map(list_pkg, function(pkg){
   usethis::use_package(pkg)  
 })
