912d7bf71256f75582fdee0614a171ff926123ed
CamilaAchuri <camilaachury.4@gmail.com>
2022-06-02 15:21:24 -0300

    disable filters

--- R/mod_load_parmesan.R
+++ R/mod_load_parmesan.R
@@ -299,7 +299,14 @@ mod_load_parmesan_server <- function(id, r){
     
     
     li <- reactive({
-      parmesan:::index_inputs(session = session, input = input, parmesan = parmesan) %>% plyr::compact()
+      print( input)
+      df <- parmesan:::index_inputs(session = session, input = input, parmesan = parmesan, 
+                                    disincludeInputs = c("varViewId", "desagregacionId", "aggId", "anioId", "colorsId",
+                                                         "fechasId", "stackedId", "sortBar", "axisId")) %>% plyr::compact()
+      #if (!identical(df, list())) df <- df %>% dplyr::filter(!id %in% c("desagregacionId"))
+      print("tablita")
+      print(df)
+      df
     })
     
     id_parmesan <- reactive({
